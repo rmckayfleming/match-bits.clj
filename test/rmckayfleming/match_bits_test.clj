1fb7ec2883c33854dc6a01549853c136a50b4e8f
Ryan McKay-Fleming <ryan@mckayfleming.com>
2023-05-28 16:54:43 -0400

    Examples from docs are now test cases

--- test/rmckayfleming/match_bits_test.clj
+++ test/rmckayfleming/match_bits_test.clj
@@ -2,6 +2,60 @@
   (:require [clojure.test :refer :all]
             [rmckayfleming.match-bits :refer :all]))
 
+(deftest documentation-examples
+  (testing "Exact Integers Example"
+    (is (match-bits (+ 2r1000 2r0001)
+                    2r1000 :nope
+                    2r0001 :nope
+                    2r10000001 :nope
+                    2r1001 :yes!
+                    :default)
+        :yes!))
+  (testing "Bit Masks Examples"
+    (is (match-bits (+ 2r1000 2r0001)
+                    %1000 :nope
+                    %0001 :nope
+                    %10000001 :nope
+                    %1001 :yes!
+                    :default)
+        :yes!)
+    (is (match-bits 2r10011010
+                    2r1010 :nope
+                    %1010 :yes!
+                    :default)
+        :yes!))
+  (testing "Wildcards Example"
+    (is (match-bits 2r10011010
+                    2r10010000 :nope
+                    %1001____ :yes!
+                    :default)
+        :yes!))
+  (testing "Pattern Variables Examples"
+    (is (match-bits 2r10011010
+                    2r10010000 :nope
+                    %aaaabbbb [aaaa bbbb]
+                    :default)
+        [2r1001 2r1010])
+    (is (match-bits 2r10011010
+                    2r10010000 :nope
+                    %aaabbbbb [aaa bbbbb]
+                    :default)
+        [2r100 2r11010]))
+  (testing "Fallthrough Examples"
+    (is (= (match-bits 2r10011010
+                       2r10010000 :nope
+                       (do (println "Default reached!")
+                           :yes!))
+         :yes!))
+    (is (= (with-out-str
+             (match-bits 2r10011010
+                         2r10010000 :nope
+                         (do (println "Default reached!")
+                             :yes!)))
+           "Default reached!\n"))
+    (is (nil? (match-bits 2r10011010
+                          2r10010000 :nope)))))
+
 (deftest exact-matches
   (testing "Matches exact numbers"
     (is (match-bits 2r1001
